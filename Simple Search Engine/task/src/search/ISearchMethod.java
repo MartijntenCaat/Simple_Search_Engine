ae0b9f0f5090709e70d570f0463c67d25a8bbde5
Martijn ten Caat <martijn.tencaat@edsn.nl>
2020-09-09 20:34:37 +0200

    Added javadoc

--- Simple Search Engine/task/src/search/ISearchMethod.java
+++ Simple Search Engine/task/src/search/ISearchMethod.java
@@ -2,6 +2,9 @@ package search;
 
 import java.util.ArrayList;
 
+/**
+ * Interface for implementation of n search algorithms.
+ */
 public interface ISearchMethod {
     ArrayList<String> searchFor(String[] queryParts, SearchIndex searchIndex);
 }
