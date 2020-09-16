daf2c86d260047ea72257599c392585ea2940a48
Martijn ten Caat <martijn.tencaat@edsn.nl>
2020-09-16 07:49:20 +0200

    Added JavaDoc

--- Simple Search Engine/task/src/search/ISearchMethod.java
+++ Simple Search Engine/task/src/search/ISearchMethod.java
@@ -6,5 +6,13 @@ import java.util.ArrayList;
  * Interface for implementation of n search algorithms.
  */
 public interface ISearchMethod {
+
+    /**
+     * Method to be implemented in every subclass of a different search algoritm
+     *
+     * @param queryParts  incoming query split up in parts to be searched for
+     * @param searchIndex the total index to be searched in
+     * @return results of the search in a list
+     */
     ArrayList<String> searchFor(String[] queryParts, SearchIndex searchIndex);
 }
