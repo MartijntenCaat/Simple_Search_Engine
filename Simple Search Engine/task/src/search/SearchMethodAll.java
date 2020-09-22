ca16c490083e8971a48155bc5dbe5ced4db495e7
Martijn ten Caat <martijn.tencaat@edsn.nl>
2020-09-22 13:50:14 +0200

    Added JavaDoc and used a better for loop

--- Simple Search Engine/task/src/search/SearchMethodAll.java
+++ Simple Search Engine/task/src/search/SearchMethodAll.java
@@ -8,6 +8,7 @@ public class SearchMethodAll implements ISearchMethod {
 
     /**
      * If the strategy is ALL, the program should print lines containing all words from the query.
+     * If at least one query part is not present in search index the result should be empty.
      *
      * @param queryParts from user input to be searched in search index.
      * @return result list with zero, one or more results in the search index.
@@ -16,14 +17,14 @@ public class SearchMethodAll implements ISearchMethod {
     public ArrayList<String> searchFor(String[] queryParts, SearchIndex searchIndex) {
         result = new ArrayList<>();
 
-        for (int i = 0; i < queryParts.length; i++) {
-
-            if (searchIndex.getInvertedSearchIndex().containsKey(queryParts[i])) {
-                for (int index : searchIndex.getInvertedSearchIndex().get(queryParts[i])) {
+        for (String queryPart : queryParts) {
+            if (searchIndex.getInvertedSearchIndex().containsKey(queryPart)) {
+                for (int index : searchIndex.getInvertedSearchIndex().get(queryPart)) {
                     result.add(searchIndex.getRawSearchIndex().get(index));
                 }
             } else {
-                return new ArrayList<>();
+                result.clear();
+                return result;
             }
         }
         LinkedHashSet<String> resultHashSet = new LinkedHashSet<>(result);
